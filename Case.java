b7fe4affbf88f8415b11c81adaec72a340af5672
jasoncoleman <jccbcc99@gmail.com>
2021-07-13 06:40:41 -0500

    version 0.2

--- Case.java
+++ Case.java
@@ -7,6 +7,13 @@ public class Case {
     private String powerSupply;
     private Dimensions dimensions;
 
+    public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
+    }
+
     public void pressPowerButton() {
         System.out.println("Power button pressed");
     }
